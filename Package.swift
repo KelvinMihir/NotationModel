627d17f98fa768491ee8cc318b56aee6d74cb439
Kelvin + Mihir <99100233+KelvinMihir@users.noreply.github.com>
2022-07-11 21:50:14 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .library(name: "StaffModel", targets: ["StaffModel"])
     ],
     dependencies: [
-        .package(url: "https://github.com/dn-m/Structure", from: "0.23.0"),
-        .package(url: "https://github.com/dn-m/Math", from: "0.7.1"),
-        .package(url: "https://github.com/dn-m/Music", from: "0.15.0")
+//         .package(url: "https://github.com/dn-m/Structure", from: "0.23.0"),
+//         .package(url: "https://github.com/dn-m/Math", from: "0.7.1"),
+//         .package(url: "https://github.com/dn-m/Music", from: "0.15.0")
     ],
     targets: [
         // Sources
